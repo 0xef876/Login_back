05f2d6b6953b09c410bc4a228774534121c34e3c
kittypark <kittypark.github.io@gmail.com>
2023-04-21 11:57:29 +0900

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ export PATH=$PATH:$HOME/dotnet
 
 echo "Deploying code..."
 dotnet publish
-nohup dotnet /home/ubuntu/back-end/bin/Debug/netcoreapp2.2/API.dll --urls http://0.0.0.0:8000 > /dev/null 2>&1 &
+nohup dotnet /home/ubuntu/back-end/bin/Debug/netcoreapp2.2/publish/API.dll --urls http://0.0.0.0:8000 > /dev/null 2>&1 &
 pid=$!
 while [ -e /proc/$pid ]; do sleep 1; done
 echo "Deployment complete. "
