3341b4322988d91a6bfc1ec9f0d95e7bcb0b5a46
kittypark <kittypark.github.io@gmail.com>
2023-04-21 12:32:33 +0900

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,9 +7,4 @@ export DOTNET_ROOT=$HOME/dotnet
 export PATH=$PATH:$HOME/dotnet
 export ASPNETCORE_ENVIRONMENT=Production
 export PASSWORD=$(cat /etc/password.txt)
-echo "Deploying code..."
 dotnet publish
-nohup dotnet /home/ubuntu/back-end/bin/Debug/netcoreapp2.2/publish/API.dll --urls http://0.0.0.0:8000 > /home/ubuntu/back-end/logs.txt 2>&1 &
-pid=$!
-while [ -e /proc/$pid ]; do sleep 1; done
-echo "Deployment complete."
