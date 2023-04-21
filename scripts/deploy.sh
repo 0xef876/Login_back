4a2a6112d54a2237af0f72ced5b65d65d3b44494
kittypark <kittypark.github.io@gmail.com>
2023-04-21 12:26:15 +0900

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,11 @@
 REPO=/home/ubuntu/back-end
 
 cd $REPO
+pkill dotnet
 export DOTNET_ROOT=$HOME/dotnet
 export PATH=$PATH:$HOME/dotnet
 export ASPNETCORE_ENVIRONMENT=Production
+export PASSWORD=$(cat /etc/password.txt)
 echo "Deploying code..."
 dotnet publish
 nohup dotnet /home/ubuntu/back-end/bin/Debug/netcoreapp2.2/publish/API.dll --urls http://0.0.0.0:8000 > /home/ubuntu/back-end/logs.txt 2>&1 &
