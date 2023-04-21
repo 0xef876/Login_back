81d4233b977515edb0ecd8fbb551a7e446d623e3
kittypark <kittypark.github.io@gmail.com>
2023-04-21 11:30:41 +0900

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,8 @@
 REPO=/home/ubuntu/back-end
 
 cd $REPO
+export DOTNET_ROOT=$HOME/dotnet
+export PATH=$PATH:$HOME/dotnet
 
 dotnet publish
-
 nohup dotnet /home/ubuntu/back-end/bin/Debug/netcoreapp2.2/API.dll --urls http://0.0.0.0:8000 &
